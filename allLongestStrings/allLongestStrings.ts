e10c124430136d0b11f46ba3d8111487548f2b0a
amlinden <amlinden@kth.se>
2021-02-26 17:23:15 +0100

    added longeststring
    added longest string funtion

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,10 @@
 export function allLongestStrings(inputArray: string[]): string[] {
-
+    let longestString = []
+    for (let index = 0; index < inputArray.length; index++) {
+        longestString.push(inputArray[index].length);
+    }
+    let largest = inputArray.filter(x => x.length === Math.max(...longestString));
+    return largest;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
