65d43d09eb749b93f942b2a11ea193a177d2aae7
amlinden <amlinden@kth.se>
2021-02-26 12:22:25 +0100

    added absolutevaluesum

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,13 @@
+//The median: middle score of a distribution. 
+
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    //checks if array is even or odd
+    const isEven = a.length%2 === 0;
+    //return middle number  math floor
+    return isEven ? a[a.length/2 - 1] : a[Math.floor(a.length/2)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
