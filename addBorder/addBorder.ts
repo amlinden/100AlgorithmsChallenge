931c4e7e7f2c0947ddfb560384b5d097e8c4006e
amlinden <amlinden@kth.se>
2021-02-26 14:23:10 +0100

    added function addborder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,23 @@
-export function addBorder(picture: string[]): string[] {
+export function addBorder(picture: string[]): any {
+    const width = picture[0].length + 2;
+    let matrix = "";
+    for(let i = 0;  i < width; i++){
+        matrix = matrix.concat("*")
+    }
+    picture.unshift(matrix);
+    picture.push(matrix)
+
+    for(let i = 0; i < picture.length; i++){
+        picture[i] = "*".concat(picture[i]).concat("*")
+    }
+
+    return picture;
+}
+
+console.log(addBorder(["abc", "ded"]));
+
+export function addBorder2(picture: string[]): any {
 
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder2(["abc", "ded"]));
