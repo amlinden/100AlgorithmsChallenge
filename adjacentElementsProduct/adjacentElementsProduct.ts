6bb67f44f3214f15f076e76179bb7cbdcec813dd
amlinden <amlinden@kth.se>
2021-02-26 15:54:34 +0100

    added adjecent function

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,24 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+    let largestprod: number[] = [];
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        largestprod.push(inputArray[i] * inputArray[i + 1]);
+    }
+    return Math.max(...largestprod);
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+
+
+//option 2
+
+export function adjacentElementsProduct2(inputArray: number[]): number {
+    let largestprod = inputArray[0] * inputArray[1];
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        let prod = inputArray[i] * inputArray[i + 1];
+        largestprod = largestprod > prod ? largestprod: prod;
+
+    }
+    return largestprod;
+}
+
+console.log(adjacentElementsProduct2([3, 6, -2, -5, 7, 3]));
