f3c942a394362c8bd56c391f7d493f7a05b4da53
amlinden <amlinden@kth.se>
2021-02-26 13:21:23 +0100

    Added add function

--- add/README.md
+++ add/README.md
@@ -9,6 +9,19 @@
 For param1 = 1 and param2 = 2, the output should be
 add(param1, param2) = 3.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 **Hints**
 -   Arithmetic Operators
 -   Rest Operator
--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,20 @@
 export function add(param1: number, param2: number): number {
+    return param1 + param2
 
 }
+console.log(add(1, 2));
+console.log(add(3, 2));
 
+
+//rest param when you dont know how many you will iterate through
 export function add2(...param1: number[]): number {
-
+    let totalSum = 0;
+    param1.forEach((element) => {
+        totalSum += element
+    })
+    return totalSum;
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
